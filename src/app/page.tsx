36a97ce3b73c217d4bcd396fc7c8f66bf7a9a102
Joshua Verdehem <joshverd2@gmail.com>
2025-02-20 14:44:51 -0600

    Edge runtime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import ClientComponent from './_components/ClientComponent/ClientComponent';
 
 const COOKIE_NAME = 'objectid-dayjs-expressions';
 
+export const runtime = 'edge';
+
 const Home = () => {
   const initialDayjsExpressions = JSON.parse(cookies().get(COOKIE_NAME)?.value || '[]');
 
